785622b2e825cb81ad06e794c23d1517d18c35f6
ranjithkumarm2003 <ranjithmoorthy03@gmail.com>
2025-08-17 21:45:50 +0530

    Resolved sudoku

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -11,6 +11,8 @@ public class SudokuSolver {
                 {0, 0, 9, 7, 0, 0, 0, 0, 5},
                 {0, 0, 0, 2, 0, 0, 0, 0, 0},
                 {0, 0, 7, 0, 4, 0, 2, 0, 3}
+
+
         };
         if(solver(board)){
             System.out.println("Board was Successfully Solved");
@@ -21,20 +23,21 @@ public class SudokuSolver {
         printBoard(board);
     }
     private static void printBoard(int [][]board){
-         for(int i=0;i<grid_Size;i++){
-             if(i%3==0 && i!=0){
-                 System.out.println("---- ----- ----");
-             }
-             for(int j=0;j<grid_Size;j++){
-                 if(j%3==0 && j!=0){
-                     System.out.print(" | ");
-                 }
-                 System.out.print(board[i][j]);
-             }
-             System.out.println();
-         }
+        for(int i=0;i<grid_Size;i++){
+            if(i%3==0 && i!=0){
+                System.out.println("---- ----- ----");
+            }
+            for(int j=0;j<grid_Size;j++){
+                if(j%3==0 && j!=0){
+                    System.out.print(" | ");
+                }
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
     }
-    private static boolean isNumInRow(int [][]board, int num, int row){
+
+    public static boolean rowCheck(int [][] board, int num, int row){
         for(int i=0;i<grid_Size;i++){
             if(board[row][i]==num){
                 return true;
@@ -42,7 +45,7 @@ public class SudokuSolver {
         }
         return false;
     }
-    private static boolean isNumInCol(int [][]board, int num, int col){
+    public static boolean colCheck(int [][]board, int num, int col){
         for(int i=0;i<grid_Size;i++){
             if(board[i][col]==num){
                 return true;
@@ -50,11 +53,11 @@ public class SudokuSolver {
         }
         return false;
     }
-    private static boolean isNumInBox(int [][]board, int num, int row,int col){
-        int localBoxRow=row-row%3;
-        int localBoxCol=col-col%3;
-        for(int i=localBoxRow;i<localBoxRow+3;i++){
-            for(int j=localBoxCol;j<localBoxCol+3;j++){
+    public static boolean boxCheck(int [][]board, int num, int row, int col){
+        int localRow=row-row%3;
+        int localCol=col-col%3;
+        for(int i=localRow;i<localRow+3;i++){
+            for(int j=localCol;j<localCol+3;j++){
                 if(board[i][j]==num){
                     return true;
                 }
@@ -62,24 +65,22 @@ public class SudokuSolver {
         }
         return false;
     }
-    private static boolean isValidPlacement(int [][]board, int num, int row, int col){
-        return !isNumInBox(board,num,row,col) && !isNumInRow(board,num,row) && !isNumInCol(board,num,col);
-
+    public static boolean isValidPosition(int board[][], int num, int row, int col){
+        return !rowCheck(board,num,row) && !colCheck(board,num,col) && !boxCheck(board,num,row,col);
     }
-    private static boolean solver(int [][]board){
+    public static boolean solver(int board[][]){
         for(int i=0;i<grid_Size;i++){
             for(int j=0;j<grid_Size;j++){
                 if(board[i][j]==0){
-                    for(int numToTry=1;numToTry<=9;numToTry++){
-                        if(isValidPlacement(board,numToTry,i,j)){
-                            board[i][j]=numToTry;
+                    for(int tryNum=1;tryNum<=9;tryNum++){
+                        if(isValidPosition(board, tryNum,i,j)){
+                            board[i][j]=tryNum;
                             if(solver(board)){
                                 return true;
                             }
-                            else{
-                                board[i][j]=0;
-                            }
+                            else board[i][j]=0;
                         }
+
                     }
                     return false;
                 }
